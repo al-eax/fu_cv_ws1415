e69ed7bad8fc4cf87b4a3935ba519ee2873065e0
alex <hinze.alex@googlemail.com>
2015-11-03 14:06:22 +0100

    wip

--- ub2/aufg2.py
+++ ub2/aufg2.py
@@ -1,6 +1,8 @@
 import csv
 import sys 
+import Image
 
+#read csv file
 def csv2points(path):
 	f = open(path, 'rb')
 	reader = csv.reader(f)
@@ -22,18 +24,19 @@ def csv2points(path):
 		points.append((x[p],y[p],z[p]))
 	return points 
 
-
-def world2screen(point, f):
+#world to screen by pinhole peojection
+def w2cPinhole(point, f):
 	factor = float(f)/float(point[2])
 	y = factor * point[1]
 	x = factor * point[0]
 	return (x,y)
 
+#calculate bounding box
 def getBB(points):
 	minx = points[0][0]
 	miny = points[0][1]
-	maxx = points[1][0]
-	maxy = points[1][1]
+	maxx = points[0][0]
+	maxy = points[0][1]
 
 	for point in points:
 		if point[0] > maxx:
@@ -46,13 +49,43 @@ def getBB(points):
 			miny = point[1]
 	return ( (minx,miny,) , (maxx,maxy))
 
+#project 2d points on image
+def coords2img(screenCoords,rgb):
+	#calc bounding box
+	((minx, miny), (maxx, maxy)) = getBB(screenCoords)
+	imgSize = (int((maxx - minx) + 1) , int((maxy - miny) + 1))
+	img = Image.new( 'RGB',imgSize , "black")
+	pixels = img.load()
+	#set pixels
+	for (px,py) in screenCoords:
+		pixel = (int(px - minx ), int(py - miny))
+		pixels[pixel] = rgb
+	return img	
+############ 1. img
+#read 3d points from csv
 points = csv2points("pointdata_3d.csv")
 screenCoords = []
-f = 5.0
+f = 10000.0
+#convert 3d to 2d
 for point in points:
-	screenCoords.append(world2screen(point,f))
-bb = getBB(screenCoords)
-print bb
+	screenCoords.append(w2cPinhole(point,f))
+#display 2d points
 
-bb = getBB(points)
+img1 = coords2img(screenCoords, (255,0,0))
+img1.save("img1_f" + str(f) + ".png")
 
+########## 2. img
+screenCoords = []
+f = f/2.0
+for point in points:
+	screenCoords.append(w2cPinhole(point,f))
+img2 = coords2img(screenCoords, (0,255,0))
+img2.save("img2_f" + str(f) + ".png")
+
+
+######## 3. img
+f = f*2.0
+for point in points:
+	screenCoords.append(w2cPinhole(point,f))
+img3 = coords2img(screenCoords, (0,0,255))
+img3.save("img3_f" + str(f) + ".png")
