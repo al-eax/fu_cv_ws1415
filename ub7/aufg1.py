916ac65918917809fe0515cd0f9994470d329820
Alex H <hinze.alex@googlemail.com>
2016-01-27 01:40:26 +0100

    foo

--- ub7/aufg1.py
+++ ub7/aufg1.py
@@ -17,15 +17,18 @@ def readCSV(path = "corners.cvs"):
         r.append( (int(row[0]) , int(row[1])))
     return r
 
-def getGaussians(img, scales = 4):
-    #TODO convert to float ?
+
+def getGaussians(img,currentOctave , scales = 4):
     result = []
-    kernelSize = (9,9)
+    #kernelSize = (9,9)
+    kernelSize = (5,5)
+    sigma = 1.6
     for i in range(scales):
-        sigma = pow(math.sqrt(2),i)
-        print sigma
+        #TODO beginne mit anderem Sigma !!!!!
+        k = pow(math.sqrt(2),i)
+        #k = (i+1) * math.sqrt(2)
         blur = img.copy()
-        cv2.GaussianBlur(src=img,dst = blur,ksize=kernelSize,sigmaX=sigma,sigmaY=sigma,borderType = cv2.BORDER_DEFAULT )
+        cv2.GaussianBlur(src=img,dst = blur,ksize=kernelSize,sigmaX=sigma*k,sigmaY=sigma,borderType = cv2.BORDER_DEFAULT )
         result.append(blur)
     return result
 
@@ -40,8 +43,8 @@ def showGaussians(gaus):
     for g in gaus:
         imShow(g,"Gaussian")
 
-def showDog(dog):
-    for d in dog:
+def showDogs(dogs):
+    for d in dogs:
         imShow(d,"DOG")
 
 def halveImg(img):
@@ -50,14 +53,44 @@ def halveImg(img):
 
 def getDOGPyramid(img, octaves = 4):
     k = 0
+    dogs = []
     for i in range(octaves -1):
-        g = getGaussians(img)
-        #showGaussians(g)
-        dog = getDOG(g)
-        showDog(dog)
+        g = getGaussians(img,i)
+        d = getDOG(g)
+        dogs.append(d)
         img = halveImg(img)
+        #showGaussians(g)
+        #showDogs(d)
+    return dogs
 
-img = cv2.imread("Lenna.png")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#convert to float64?
-getDOGPyramid(img)
+def getKeyPoints(doglist):
+    listOfKeypoints = []
+    for dogs in doglist:
+        keypoints = []
+        d1 = dogs[0]
+        d2 = dogs[1]
+        d3 = dogs[2]
+        final = d1.copy()
+        final[:,:] = 100
+        for start_y in range(len(d1) - 3):
+            for start_x in range(len(d1[0]) - 3):
+                minVal = (0,0,0,dogs[0][0][0])
+                maxVal = (0,0,0,dogs[0][0][0])
+                for x in range(start_x , start_x + 3):
+                    for y in range(start_y , start_y + 3):
+                        for z in range(3):
+                            if (minVal[3] < dogs[z][y][x]):
+                                minVal = (z,y,x,dogs[z][y][x])
+                            if(maxVal[3] > dogs[z][y][x]):
+                                maxVal = (z,y,x,dogs[z][y][x])
+                final[maxVal[1]][maxVal[2]] = float(200.0)
+                final[maxVal[1]][maxVal[2]] = float(0.0)
+        imShow(final)
+        listOfKeypoints.append(keypoints)
+
+img = cv2.imread("Lenna.png") #read image
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to gray
+img = np.float64(img) #convert to float
+
+dogs = getDOGPyramid(img)
+getKeyPoints(dogs)
